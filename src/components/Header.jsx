bda26ded285069f1f9af6be1df320d3423180282
Devesh Wadibhasme <146552994+deveshwadibhasme@users.noreply.github.com>
2024-12-01 22:17:56 +0530

    all set

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router"
+
 const Header = () => {
   return (
     <header className="max-w-screen-xl w-full min-h-16 flex items-center justify-center shadow-lg shadow-black/60 mx-auto">
         <div className="max-w-screen-lg flex w-[80%] justify-between items-center">
-            <h1 className="text-lg">Where in the world?</h1>
+            <Link to = {'/'} className="text-lg">Where in the world?</Link>
             <div id="mode" className="max-w-28 w-full flex h-full items-center justify-around cursor-pointer">
                 <i className="far fas fa-moon text-xl text-white [text-shadow:0px_0px_3px_rgb(12,_20,_23)]" title="dark or white" aria-hidden="true"></i><span className="sr-only">dark or white</span>
                 <h2 id="mode-title">
--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,45 +1,77 @@
-import { useEffect, useState } from "react";
 import Loading from "../components/LoadingCountry.jsx";
-import { useParams } from "react-router";
+import { Link, useParams } from "react-router";
 import Error from '../components/Error.jsx'
+import { useEffect, useState } from "react";
 
 const Country = () => {
   const countryName = useParams()
   const [country, setCountry] = useState(null)
 
+
   useEffect(() => {
     fetch(`https://restcountries.com/v3.1/name/${countryName.country}`)
       .then((response) => response.json())
       .then((data) => {
-        setCountry(data[0]
-        ).catch(()=>{<Error/>})
-      }, [])
-
+        setCountry(
+          {
+            image: data[0].flags.svg,
+            name: data[0].name.common,
+            population: data[0].population.toLocaleString('en-IN'),
+            region: data[0].region,
+            subregion: data[0].subregion,
+            capital: data[0].capital == null ? 'None' : (data[0].capital).join(', '),
+            tld: (data[0].tld).join(', '),
+            currencies: data[0].currencies == null ? 'None' : (Object.values(data[0].currencies)[0].name),
+            language: data[0].languages == null ? 'None' : (Object.values(data[0].languages).join(', ')),
+            borders: []
+          }
+        )
+        console.log(data.borders);
+        data[0].borders.map((border) => {
+          fetch(`https://restcountries.com/v3.1/alpha/${border}`)
+            .then((res) => res.json())
+            .then((bordData) => {
+              setCountry((preState) => {
+                return (
+                  { ...preState, borders: [...preState.borders, bordData[0].name.common] }
+                )
+              })
+            })
+        })
+      }).catch(() => { <Error /> })
     return () => {
       // cleanup code here
     }
-  }, [])
+  }, [countryName])
+
 
   return (
-    country === null ? <Loading/>
-   : (
-   
-   <div className='max-w-6xl min-h-[470px] mx-auto flex items-center relative justify-center md:justify-normal flex-col md:flex-row mt-5'>
-    <button type="button" onClick={()=>history.back()} className="absolute top-0 left-5 px-4 py-1 bg-slate-700 text-white">&larr; Back</button>
-      <div className="w-[40%] mx-auto md:mx-0">
-        <img src={country.flags.svg} alt="" />  
-      </div>
-      <div className="mx-auto md:ml-10  self-start mt-5 text-center md:text-left">
-        <h1 className="text-5xl mb-6"><b>{country.name.common}</b></h1>
-        <h2><b>Population:</b> {country.population.toLocaleString('en-IN')}</h2>
-        <h2><b>Region</b>: {country.region}</h2>
-        <h2><b>Subregion</b>: {country.subregion}</h2>
-        <h2><b>Capital</b>: {country.capital==null ? 'None' : (country.capital).join(', ')}</h2>
-        <h2><b>Top Level Domain</b>: {(country.tld).join(', ')}</h2>
-        <h2><b>Currencies</b>: {country.currencies == null ? 'None' : (Object.values(country.currencies)[0].name)}</h2>
-        <h2><b>Language</b>: {country.languages == null ? 'None' : (Object.values(country.languages).join(', '))}</h2>
-      </div>
-    </div>)
+    country === null ? <Loading />
+      : (
+        <div className='max-w-6xl min-h-[470px] mx-auto flex items-center relative justify-center md:justify-normal flex-col md:flex-row mt-5'>
+          <button type="button" onClick={() => history.back()} className="absolute top-0 left-5 px-4 py-1 bg-slate-700 text-white">&larr; Back</button>
+          <div className="w-[40%] mx-auto md:mx-0">
+            <img src={country.image} alt="" />
+          </div>
+          <div className="mx-auto md:ml-10  self-start mt-5 text-center md:text-left">
+            <h1 className="text-5xl mb-6"><b>{country.name}</b></h1>
+            <h2><b>Population:</b> {country.population}</h2>
+            <h2><b>Region</b>: {country.region}</h2>
+            <h2><b>Subregion</b>: {country.subregion}</h2>
+            <h2><b>Capital</b>: {country.capital}</h2>
+            <h2><b>Top Level Domain</b>: {country.tld}</h2>
+            <h2><b>Currencies</b>: {country.currencies}</h2>
+            <h2><b>Language</b>: {country.language}</h2>
+            {country.borders.length !== 0 &&
+              <div className="m-2">
+                <h2><b>Border Countries</b></h2>
+                <div className="flex flex-wrap gap-2 m-2">
+                  {country.borders.map((bord) => <Link className="shadow-sm shadow-black/60 p-2" key={bord} to={'/' + bord }>{bord}</Link>
+                  )}
+                </div>
+              </div>}
+          </div>
+        </div>)
   )
 }
 
