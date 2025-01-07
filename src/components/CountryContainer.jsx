88e3a1ae81bfb3fd707786e0ec8c11b4ef989960
Devesh Wadibhasme <deveshwadibhasme.12@gmail.com>
2025-01-07 22:23:08 +0530

    async added

--- src/components/CountryContainer.jsx
+++ src/components/CountryContainer.jsx
@@ -1,61 +1,83 @@
-import { useEffect, useState } from 'react';
-import CountryCard from './CountryCard.jsx';
-import Loading from './CountryLoading.jsx';
+import { useEffect, useState } from "react";
+import CountryCard from "./CountryCard.jsx";
+import Loading from "./CountryLoading.jsx";
 
+const CountryContainer = ({ query, region }) => {
+  const [CountryData, setCountryData] = useState(null);
+  const [regionData, setRegionData] = useState([]);
 
-const CountryContainer = ({query,region}) => {
-    
-    const [CountryData,setCountryData] = useState(null)
-    const [regionData,setRegionData] = useState([])
+  useEffect(() => {
+    async function fetchAllCountry() {
+      const response = await fetch("https://restcountries.com/v3.1/all");
+      const data = await response.json();
+      setCountryData(data);
+    }
+    fetchAllCountry();
+    return () => {
+      // cleanup code here
+    };
+  }, []);
 
-    useEffect(() => {
-      fetch('https://restcountries.com/v3.1/all')
-      .then((response)=>response.json())
-      .then((data) =>setCountryData(data))
+  useEffect(() => {
+    if (region && region.trim() !== "") {
+      fetch("https://restcountries.com/v3.1/region/" + region)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          } else {
+            return response.json();
+          }
+        })
+        .then((data) => setRegionData(data))
+        .catch((error) => console.error("Error fetching region data:", error));
+    } else {
+      console.log("Please enter a valid region.");
+      setRegionData([]);
       return () => {
         // cleanup code here
-      }
-    },[])
-     
-    useEffect(() => {
-        if (region && region.trim() !== '') {  
-            fetch('https://restcountries.com/v3.1/region/' + region)
-                .then((response) => {
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok');
-                    }else{
-                        return response.json();
-                    }
-                })
-                .then((data) => setRegionData(data))
-                .catch((error) => console.error('Error fetching region data:', error));
-        } else {
-            console.log("Please enter a valid region.");
-            setRegionData([])
-            return () => {
-                // cleanup code here
-              }
-        }
-    }, [region]); 
-
-    function checkEmpty() {
-        return regionData.length === 0 ? true : false;
+      };
     }
-    
- 
-    return (
-        CountryData === null ? <Loading/> : <div className='grid grid-cols-[repeat(2,180px)] md:grid-cols-[repeat(4,280px)] mx-[auto] my-[0] max-w-[1160px] justify-center w-full gap-[10px]'>
-            {checkEmpty() ? 
-            CountryData.filter((eve)=>eve.name.common.toLowerCase().includes(query)).map((country) => {
-                return <CountryCard key = {country.name.common} name={country.name.common} flag={country.flags.svg} population={(country.population).toLocaleString('en-IN')} region={country.region} capital={country.capital} />
-            })
-             : 
-                regionData.filter((eve)=>eve.name.common.toLowerCase().includes(query)).map((country) => {
-                    return <CountryCard key = {country.name.common} name={country.name.common} flag={country.flags.svg} population={(country.population).toLocaleString('en-IN')} region={country.region} capital={country.capital} />
-                })
-            }
-        </div>
-    )
-}
+  }, [region]);
 
-export default CountryContainer
+  function checkEmpty() {
+    return regionData.length === 0 ? true : false;
+  }
+
+  return CountryData === null ? (
+    <Loading />
+  ) : (
+    <div className="grid grid-cols-[repeat(2,180px)] md:grid-cols-[repeat(4,280px)] mx-[auto] my-[0] max-w-[1160px] justify-center w-full gap-[10px]">
+      {checkEmpty()
+        ? CountryData.filter((eve) =>
+            eve.name.common.toLowerCase().includes(query)
+          ).map((country) => {
+            return (
+              <CountryCard
+                key={country.name.common}
+                name={country.name.common}
+                flag={country.flags.svg}
+                population={country.population.toLocaleString("en-IN")}
+                region={country.region}
+                capital={country.capital}
+              />
+            );
+          })
+        : regionData
+            .filter((eve) => eve.name.common.toLowerCase().includes(query))
+            .map((country) => {
+              return (
+                <CountryCard
+                  key={country.name.common}
+                  name={country.name.common}
+                  flag={country.flags.svg}
+                  population={country.population.toLocaleString("en-IN")}
+                  region={country.region}
+                  capital={country.capital}
+                />
+              );
+            })}
+    </div>
+  );
+};
+
+export default CountryContainer;
--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,78 +1,121 @@
 import Loading from "../components/LoadingCountry.jsx";
 import { Link, useParams } from "react-router";
-import Error from '../components/Error.jsx'
+import Error from "../components/Error.jsx";
 import { useEffect, useState } from "react";
 
 const Country = () => {
-  const countryName = useParams()
-  const [country, setCountry] = useState(null)
-
+  const countryName = useParams();
+  const [country, setCountry] = useState(null);
 
   useEffect(() => {
-    fetch(`https://restcountries.com/v3.1/name/${countryName.country}`)
-      .then((response) => response.json())
-      .then((data) => {
-        setCountry(
-          {
-            image: data[0].flags.svg,
-            name: data[0].name.common,
-            population: data[0].population.toLocaleString('en-IN'),
-            region: data[0].region,
-            subregion: data[0].subregion,
-            capital: data[0].capital == null ? 'None' : (data[0].capital).join(', '),
-            tld: (data[0].tld).join(', '),
-            currencies: data[0].currencies == null ? 'None' : (Object.values(data[0].currencies)[0].name),
-            language: data[0].languages == null ? 'None' : (Object.values(data[0].languages).join(', ')),
-            borders: []
+    async function fetchCountry() {
+      const response = await fetch(
+        `https://restcountries.com/v3.1/name/${countryName.country}`
+      );
+      const data = await response.json();
+      setCountry({
+        image: data[0].flags.svg,
+        name: data[0].name.common,
+        population: data[0].population.toLocaleString("en-IN"),
+        region: data[0].region,
+        subregion: data[0].subregion,
+        capital: data[0].capital == null ? "None" : data[0].capital.join(", "),
+        tld: data[0].tld.join(", "),
+        currencies:
+          data[0].currencies == null
+            ? "None"
+            : Object.values(data[0].currencies)[0].name,
+        language:
+          data[0].languages == null
+            ? "None"
+            : Object.values(data[0].languages).join(", "),
+        borders: [],
+      });
+
+      data[0].borders
+        .map((border) => {
+          async function fetchBorder() {
+            const response = await fetch(
+              `https://restcountries.com/v3.1/alpha/${border}`
+            );
+            const data = await response.json();
+            setCountry((preState) => {
+              return {
+                ...preState,
+                borders: [...preState.borders, data[0].name.common],
+              };
+            });
           }
-        )
-        console.log(data.borders);
-        data[0].borders.map((border) => {
-          fetch(`https://restcountries.com/v3.1/alpha/${border}`)
-            .then((res) => res.json())
-            .then((bordData) => {
-              setCountry((preState) => {
-                return (
-                  { ...preState, borders: [...preState.borders, bordData[0].name.common] }
-                )
-              })
-            })
+          fetchBorder();
+
         })
-      }).catch(() => { <Error /> })
+      }
+    fetchCountry();
     return () => {
       // cleanup code here
-    }
-  }, [countryName])
+    };
+  }, [countryName]);
 
-
-  return (
-    country === null ? <Loading />
-      : (
-        <div className='max-w-6xl min-h-[470px] mx-auto flex items-center relative justify-center md:justify-normal flex-col md:flex-row mt-5'>
-          <button type="button" onClick={() => history.back()} className="absolute top-0 left-5 px-4 py-1 bg-slate-700 text-white">&larr; Back</button>
-          <div className="w-[40%] mx-auto md:mx-0">
-            <img src={country.image} alt="" />
+  return country === null ? (
+    <Loading />
+  ) : (
+    <div className="max-w-6xl min-h-[470px] mx-auto flex items-center relative justify-center md:justify-normal flex-col md:flex-row mt-5">
+      <button
+        type="button"
+        onClick={() => history.back()}
+        className="absolute top-0 left-5 px-4 py-1 bg-slate-700 text-white"
+      >
+        &larr; Back
+      </button>
+      <div className="w-[40%] mx-auto md:mx-0">
+        <img src={country.image} alt="" />
+      </div>
+      <div className="mx-auto md:ml-10  self-start mt-5 text-center md:text-left">
+        <h1 className="text-5xl mb-6">
+          <b>{country.name}</b>
+        </h1>
+        <h2>
+          <b>Population:</b> {country.population}
+        </h2>
+        <h2>
+          <b>Region</b>: {country.region}
+        </h2>
+        <h2>
+          <b>Subregion</b>: {country.subregion}
+        </h2>
+        <h2>
+          <b>Capital</b>: {country.capital}
+        </h2>
+        <h2>
+          <b>Top Level Domain</b>: {country.tld}
+        </h2>
+        <h2>
+          <b>Currencies</b>: {country.currencies}
+        </h2>
+        <h2>
+          <b>Language</b>: {country.language}
+        </h2>
+        {country.borders.length !== 0 && (
+          <div className="m-2">
+            <h2>
+              <b>Border Countries</b>
+            </h2>
+            <div className="flex flex-wrap gap-2 m-2">
+              {country.borders.map((bord) => (
+                <Link
+                  className="shadow-sm shadow-black/60 p-2"
+                  key={bord}
+                  to={"/" + bord}
+                >
+                  {bord}
+                </Link>
+              ))}
+            </div>
           </div>
-          <div className="mx-auto md:ml-10  self-start mt-5 text-center md:text-left">
-            <h1 className="text-5xl mb-6"><b>{country.name}</b></h1>
-            <h2><b>Population:</b> {country.population}</h2>
-            <h2><b>Region</b>: {country.region}</h2>
-            <h2><b>Subregion</b>: {country.subregion}</h2>
-            <h2><b>Capital</b>: {country.capital}</h2>
-            <h2><b>Top Level Domain</b>: {country.tld}</h2>
-            <h2><b>Currencies</b>: {country.currencies}</h2>
-            <h2><b>Language</b>: {country.language}</h2>
-            {country.borders.length !== 0 &&
-              <div className="m-2">
-                <h2><b>Border Countries</b></h2>
-                <div className="flex flex-wrap gap-2 m-2">
-                  {country.borders.map((bord) => <Link className="shadow-sm shadow-black/60 p-2" key={bord} to={'/' + bord }>{bord}</Link>
-                  )}
-                </div>
-              </div>}
-          </div>
-        </div>)
-  )
-}
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default Country
+export default Country;
